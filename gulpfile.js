84e89dbf0da374e9bd38c32c3dc15278f15c7047
danil <danil.gins72829@gmail.com>
2024-07-29 13:05:14 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+//const browserSync = require('browser-sync');
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+import cleanCSS from 'gulp-clean-css';
+//const cleanCSS = require('gulp-clean-css');
+
+import autoprefixer from 'gulp-autoprefixer';
+//const rename = require("gulp-rename");
+import rename from 'gulp-rename';
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer({
+			cascade: false
+		}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
